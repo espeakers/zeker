691c171d4a2c334f3e3ac86b099cf05947375070
Matthew Wright <mwright@espeakers.com>
2015-10-23 11:27:51 -0600

    adding an example project

---
+++ example/public/index.php
@@ -0,0 +1,6 @@
+<?php
+$use_dev = true;
+$assets_version = 1445621094251;
+?>
+<link rel="stylesheet" href="css/main<?= $use_dev ? "" : ".min"; ?>.css?v=<?= $assets_version; ?>">
+<script src="js/main<?= $use_dev ? "" : ".min"; ?>.js?v=<?= $assets_version; ?>"></script>
